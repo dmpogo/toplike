3df08949f587e0b6dbc16977d820d2e0369c473b
Nelson Knutson <nknutson@ualberta.ca>
2011-02-09 15:00:11 -0700

    Cleaned up code made sure up to date as of Feb/9/2011 for use on other computers

--- run_topmarg.sh
+++ run_topmarg.sh
@@ -12,11 +12,12 @@ inifile=${2}
 
 Out_File='../Output/Likelihood/Spherical/'${CTpp}'_epsil'${3}'_topmarg.out'
 #Prints output to file not to screen
-
+echo 'Started Ok'${1}
 ./topmarg ${inifile} 2>$Out_File<< EOF
 ../Output/CTpp_theory/${In_Dir}/${CTpp}
 ../Output/CTpp_maps/${Map_Dir}/${CTpp}_epsil${3}
 EOF
+echo 'DONE'
 
 # read seed value and if noise from ini file then tail the output file so that lable it fake_map_(noise or nonoise)_seedvalue.fits
 #mv fake_test_noise.fits ../../Ctpp_map_Realizations 
