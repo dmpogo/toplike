2d7da1da821e148d5148b4721160c32ca76d3c89
Dmitri Pogosyan <pogosyan@ualberta.ca>
2013-05-06 19:17:50 -0600

    Improvement to collect.sh

--- collect.sh
+++ collect.sh
@@ -3,7 +3,7 @@
 space=${1:?"First parameter must define data directory"}
 
 cd ${space}/Likelihood
-files=`ls -1 *${2:-}*all*${3:-}*${4:-}`
+files=`ls -1 *${2:-}*all*${3:-}*${4:-}*`
 for file in $files
 do
    awk -f ${HOME}/work/cmb/topology/Likelihood/collect.awk $file 
