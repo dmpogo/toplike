08816da53654a8394fcbe806f1038c66206a41e4
Dmitri Pogosyan <pogosyan@ualberta.ca>
2014-01-24 23:02:26 -0700

    Cleaning lm_rotate.f90.   Rotation works, noise to be checked
    Note: eigenvectors of degenerate eigenvalues have run-dependent sign,
    which affects random maps (should not affect likelihood)

--- lm_rotate.f90
+++ lm_rotate.f90
@@ -54,7 +54,7 @@ contains
     REAL(DP),    intent(in) :: a, b, g
     LOGICAL,     intent(in) :: redo_dlmm
     
-    INTEGER                                         :: npix, l, m, mp, p, ip, indl
+    INTEGER                                         :: npix,l,m,mp,p,iev,indl
     REAL(DP),    DIMENSION(0:12*nside**2-1, 1:npol) :: map
     COMPLEX(DP), DIMENSION(1:npol, 0:lmax, 0:lmax)  :: alm
     COMPLEX(DP), DIMENSION(0: lmax)                 :: ea,eg
@@ -84,22 +84,22 @@ contains
 
     npix=nside2npix(nside)
 
-!$OMP PARALLEL DO DEFAULT(SHARED),SCHEDULE(STATIC), PRIVATE(p,map,alm,indl,l,m,mp,sgn)
+!$OMP PARALLEL DO DEFAULT(SHARED),SCHEDULE(STATIC), PRIVATE(iev,p,map,alm,indl,l,m,mp,sgn)
 
-    DO p = 0,n_evalues-1
+    DO iev = 0,n_evalues-1
 
 !$OMP CRITICAL
-!test       write(0,*)'---After rotation-------', p
+!test       write(0,*)'---After rotation-------', iev
        alm = DCMPLX(0.d0, 0.d0)
        DO l = 0, lmax
           indl = l**2 + l
           DO m = 0, l
-             alm(:, l, m) = ea(m) * dlmm(indl + m, indl) * cplm(:, indl, p)  ! mp=0
+             alm(:, l, m) = ea(m) * dlmm(indl + m, indl) * cplm(:, indl, iev)  ! mp=0
              sgn = -1.d0
              DO mp = 1, l
                 alm(:, l, m) = alm(:, l, m) + ea(m) * &
-                     (dlmm(indl+m,indl+mp)*cplm(:,indl+mp,p)*eg(mp) + &
-                     sgn*dlmm(indl+m,indl-mp)*CONJG(cplm(:,indl+mp,p)*eg(mp)))
+                     (dlmm(indl+m,indl+mp)*cplm(:,indl+mp,iev)*eg(mp) + &
+                     sgn*dlmm(indl+m,indl-mp)*CONJG(cplm(:,indl+mp,iev)*eg(mp)))
                 sgn = -sgn
              ENDDO
           ENDDO
@@ -113,8 +113,8 @@ contains
           CALL alm2map(nside, lmax, lmax, alm, map)
        ENDIF
 
-       DO ip=1,npol
-          ctpp((ip-1)*npix:ip*npix-1,p) = map(:,ip)
+       DO p=1,npol
+          ctpp((p-1)*npix:p*npix-1,iev) = map(:,p)
        ENDDO
 !$OMP END CRITICAL
 
@@ -138,11 +138,11 @@ contains
     REAL(DP), intent(in), optional, DIMENSION(:)  :: kernel
     LOGICAL,  intent(in), optional                :: cut_md
 
-    COMPLEX(DP), DIMENSION(1:npol, 0:lmax, 0:lmax)   :: alm
-    REAL(DP),    DIMENSION(0:12*nside**2 - 1,1:npol) :: map
-    REAL(DP),    DIMENSION(2)                        :: zbounds=(/0.0_dp, 0.0_dp/)
-    INTEGER(I4B)                                     :: npix, l, m, p, ip, indl, lmin
-    INTEGER(I4B), PARAMETER                          :: iter_order=5
+    COMPLEX(DP), DIMENSION(1:npol,0:lmax,0:lmax)  :: alm
+    REAL(DP),    DIMENSION(0:12*nside**2-1,1:npol):: map
+    REAL(DP),    DIMENSION(2)                     :: zbounds=(/0.0_dp, 0.0_dp/)
+    INTEGER(I4B)                                  :: npix, l, m, p, ip, iev, indl, lmin
+    INTEGER(I4B), PARAMETER                       :: iter_order=5
     
     ! Cut out the monopole and dipole if requested.
     IF (present(cut_md).and.cut_md) THEN
@@ -151,7 +151,7 @@ contains
       lmin = 0
     END IF
 
-    allocate( cplm(1:npol,0:lmax*(lmax+2),0:n_evalues-1) )
+    if (.not.allocated(cplm) ) allocate( cplm(1:npol,0:lmax*(lmax+2),0:n_evalues-1) )
     cplm = DCMPLX(0.d0,0.d0)
    
     ! Precompute plm's   (should check if it is faster)
@@ -164,15 +164,15 @@ contains
 
     npix=nside2npix(nside)
 
-!$OMP PARALLEL DO DEFAULT(SHARED),SCHEDULE(STATIC), PRIVATE(ip,p,map,alm,indl,l,m)
-    DO p = 0, n_evalues - 1
+!$OMP PARALLEL DO DEFAULT(SHARED),SCHEDULE(STATIC), PRIVATE(iev,ip,p,map,alm,indl,l,m)
+    DO iev = 0, n_evalues - 1
 
 !$OMP CRITICAL       
-       do ip=1,npol
-          map(:,ip) = ctpp((ip-1)*npix:ip*npix-1,p)
+       do p=1,npol
+          map(:,p) = ctpp((p-1)*npix:p*npix-1,iev)
        enddo
        IF (n_plm .ne. 0) THEN
-          CALL map2alm_iterative(nside,lmax,lmax,iter_order,map,alm,zbounds,w8ring,plm(0:n_plm-1,1:1))
+          CALL map2alm_iterative(nside,lmax,lmax,iter_order,map,alm,zbounds,w8ring,plm(0:n_plm-1,1:npol))
        ELSE
           CALL map2alm_iterative(nside,lmax,lmax,iter_order,map,alm,zbounds,w8ring)
        ENDIF
@@ -186,7 +186,7 @@ contains
        DO l = lmin, lmax
           indl = l**2 + l
           DO m = 0, l
-             cplm(:, indl+m, p) =  alm(:, l, m)
+             cplm(:, indl+m, iev) =  alm(:, l, m)
           ENDDO
        ENDDO
 !$OMP END CRITICAL       
